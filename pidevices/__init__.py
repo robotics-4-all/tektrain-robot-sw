e11af1ac714ea26b36e1addd9fd86ec1fd530976
Iasonas Paraskevopoulos <iaswnparaskev@gmail.com>
2019-06-10 17:29:07 +0300

    Abstract hardware interface

--- pidevices/__init__.py
+++ pidevices/__init__.py
@@ -1,1 +1,3 @@
+from .hardwareInterfaces import HardwareInterface
+
 name = 'pidevices'
---
+++ pidevices/hardwareInterfaces.py
@@ -0,0 +1,24 @@
+class HardwareInterface(object):
+    """Abstract class representing a hardware interface
+
+    Methods:
+        initilize: Initialize hardware resources.
+        read: Read from the hardware. Descendants should ovevwrite this method.
+        write: Write to rhe hardware. Descendants should overwrite this method.
+        close: Free hardware resources.
+    """
+    
+    def initialize():
+        """Initialize hardware resources."""
+        pass
+
+    def read():
+        """Read data from hardware"""
+        raise NotImplemented("This method has not been implemented.")
+
+    def write():
+        """Write data to hardware"""
+        raise NotImplemented("This method has not been implemented.")
+
+    def close():
+        """Free hardware resources."""
