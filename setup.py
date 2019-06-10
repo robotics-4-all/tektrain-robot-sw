0a745171bda65f84ba277c5001f045e52397528e
Iasonas Paraskevopoulos <iaswnparaskev@gmail.com>
2019-06-10 16:07:49 +0300

    Initial package commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+REQUIRES = []
+NAME = "pidevices"
+VERSION = "0.0.1"
+
+setup(
+    name=NAME,
+    version=VERSION,
+    packages=find_packages(),
+
+    # Install required packages
+    install_requires=REQUIRES,
+
+    # Metadata
+    author=" ",
+    author_email=" ",
+    description="Drivers for sensors and actuators for the raspberry pi board.",
+    url=" ", 
+)
