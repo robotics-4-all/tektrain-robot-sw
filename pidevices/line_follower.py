0ea51aa0cac87a96fb0869935acf20d32bf3eced
Iasonas Paraskevopoulos <iaswnparaskev@gmail.com>
2019-06-17 13:31:23 +0000

    Add base line follower class

---
+++ pidevices/line_follower.py
@@ -0,0 +1,28 @@
+from .devices import Sensor
+
+class LineFollower(Sensor):
+    """Base class representing line following sensors."""
+
+    def _get_max_freq(self):
+        return self._max_frequency 
+
+    def _set_max_freq(self, freq):
+        self._max_frequency = freq
+
+    max_freq = property(_get_max_freq, _set_max_freq)
+
+    def _get_min_dist(self):
+        return self._min_dist
+
+    def _set_min_dist(self, max_dist):
+        self._min_dist = max_dist
+
+    min_dist = property(_get_min_dist, _set_min_dist)
+
+    def _get_min_dist(self):
+        return self._min_dist
+
+    def _set_min_dist(self, min_dist):
+        self._min_dist = min_dist
+
+    max_dist = property(_get_max_dist, _set_max_dist)
