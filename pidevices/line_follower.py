9f2a6d69ef889a7aac525ab8b68f4f9ba1620021
Iasonas Paraskevopoulos <iaswnparaskev@gmail.com>
2019-06-17 13:34:26 +0000

    Add constructor.

--- pidevices/line_follower.py
+++ pidevices/line_follower.py
@@ -3,13 +3,18 @@ from .devices import Sensor
 class LineFollower(Sensor):
     """Base class representing line following sensors."""
 
+    def __init__(self, max_frequency, min_dist, max_dist):
+        self._max_frequency = max_frequency
+        self._min_dist = min_dist
+        self._max_dist = max_dist
+
     def _get_max_freq(self):
         return self._max_frequency 
 
     def _set_max_freq(self, freq):
         self._max_frequency = freq
 
-    max_freq = property(_get_max_freq, _set_max_freq)
+    max_frequency = property(_get_max_freq, _set_max_freq)
 
     def _get_min_dist(self):
         return self._min_dist
