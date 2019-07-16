623c079b120480da28e8c9b34858b8361962f7b3
Iasonas Paraskevopoulos <iaswnparaskev@gmail.com>
2019-07-16 12:29:32 +0100

    Add bounce time

--- pidevices/sensors/button.py
+++ pidevices/sensors/button.py
@@ -17,9 +17,8 @@ class Button(Sensor):
 
         self._gpio = self.init_interface('gpio', button=self.pin_num)
         self.hardware_interfaces[self._gpio].init_input('button', 'up')
-        #self.hardware_interfaces[self._gpio].set_pin_bounce = 0
+        self.hardware_interfaces[self._gpio].set_pin_bounce('button', 200)
         self.hardware_interfaces[self._gpio].set_pin_edge('button', 'falling')
-        #self.hardware_interfaces[self._gpio].set_pin_event('button', self.read)
     
     def read(self, func, *args):
         self.hardware_interfaces[self._gpio].set_pin_event('button', func, *args)
--- tests/test_button.py
+++ tests/test_button.py
@@ -2,14 +2,17 @@ import unittest
 import time
 from pidevices.sensors.button import Button
 
-
+c = 0
 class TestButton(unittest.TestCase):
 
     def test_read(self):
-        def test():
-            print("skata")
+        def test(a1, a2):
+            global c
+            print("{} {} skata {}".format(c, a1, a2))
+            c += 1
         
-        button = Button(23, test)
+        button = Button(23)
+        button.read(test, 10, "na fas")
         while True:
             time.sleep(1)
 
