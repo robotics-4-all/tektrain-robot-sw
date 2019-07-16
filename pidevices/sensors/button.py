48bb85508f05ca697ef3cc02c4a57fc6c98a67ce
Iasonas Paraskevopoulos <iaswnparaskev@gmail.com>
2019-07-16 12:04:14 +0100

    First working button

--- pidevices/sensors/button.py
+++ pidevices/sensors/button.py
@@ -9,7 +9,9 @@ class Button(Sensor):
 
         super(Button, self).__init__(name, max_data_length)
         self._pin_num = pin_num
-        self.read = func
+        self.func = func
+
+        self.start()
 
     def start(self):
         """Init hardware and os resources."""
@@ -17,8 +19,11 @@ class Button(Sensor):
         self._gpio = self.init_interface('gpio', button=self.pin_num)
         self.hardware_interfaces[self._gpio].init_input('button', 'up')
         #self.hardware_interfaces[self._gpio].set_pin_bounce = 0
-        self.hardware_interfaces[self._gpio].set_pin_edge('button', 'rising')
+        self.hardware_interfaces[self._gpio].set_pin_edge('button', 'falling')
         self.hardware_interfaces[self._gpio].set_pin_event('button', self.read)
+    
+    def read(self, channel):
+        self.func()
 
     def stop(self):
         """Free hardware and os resources."""
