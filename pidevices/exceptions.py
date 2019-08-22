87e8791c566c7355cd4c5cb89dee95a90f6707d9
Iasonas Paraskevopoulos <iaswnparaskev@gmail.com>
2019-08-22 13:56:37 +0300

    Add invalid hpwm pin exception.

--- pidevices/exceptions.py
+++ pidevices/exceptions.py
@@ -1,12 +1,19 @@
 """The exceptions of the library."""
 
+
 class PidevicesError(Exception):
     """Base class for all exceptions in pidevices."""
+
 
 class NotSupportedInterface(PidevicesError):
     """Error when an invalid interface name is used."""
 
+
 class NotInstalledInterface(PidevicesError):
     """Error when there isn't any supported library installed for the 
        currrent interface."""
+
+
+class InvalidHPWMPin(PidevicesError):
+    """Error with invalid hardware pwm pin."""
     pass
