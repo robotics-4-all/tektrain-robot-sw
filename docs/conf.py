aa22ad5882ab5675c86139b311ce8c9366939bab
Iasonas Paraskevopoulos <iaswnparaskev@gmail.com>
2019-09-18 16:07:39 +0300

    Change to readthedocs theme.

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,8 @@
 #
 import os
 import sys
+import sphinx_rtd_theme
+
 # sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 
@@ -28,7 +30,8 @@ author = 'Iasonas Paraskevopoulos'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.napoleon',
+              'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -47,7 +50,25 @@ autodoc_default_options = {
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+
+# Read the docs style
+#html_theme_options = {
+#    'canonical_url': '',
+#    'analytics_id': 'UA-XXXXXXX-1',  # Provided by Google in your dashboard
+#    'logo_only': False,
+#    'display_version': True,
+#    'prev_next_buttons_location': 'bottom',
+#    'style_external_links': False,
+#    'vcs_pageview_mode': '',
+#    'style_nav_header_background': 'white',
+#    # Toc options
+#    'collapse_navigation': True,
+#    'sticky_navigation': True,
+#    'navigation_depth': 4,
+#    'includehidden': True,
+#    'titles_only': False
+#}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
