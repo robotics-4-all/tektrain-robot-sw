8cb31dcf526518bb8532590a7976ed4cad3608c6
Iasonas Paraskevopoulos <iaswnparaskev@gmail.com>
2019-10-02 11:40:33 +0100

    Implement a function to split pin_num argument and do the proper check

--- pidevices/mcp23x17.py
+++ pidevices/mcp23x17.py
@@ -32,68 +32,106 @@ class MCP23x17(Device):
     GPIOB = 0x13
     OLATA = 0x14
     OLATB = 0x15
-
-    _FUNCTION = {'input': 1, 'output': 0}
-    _POLARITY = {'same': 0, 'reverse': 1}
-    _PULL = {'up': 1, 'down': 0}
-
+    
     def __init__(self):
         pass
+    
+    def _get_chunk_number(self, pin_num):
+        """Split a string like "A_12" to A and 12.
+        
+        Args:
+            pin_num (str): The pin number, it must be in the form of A_x or 
+                B_x.
 
-    def set_pin_dir(self, pin_num, function, chunk='A'):
+        Returns:
+            A tuple that has the "chunk" and the number of the pin.
+
+        Raises:
+            TypeError: Error if the pin_num is not a string. Also if in A_x the
+                x is not int.
+            ValueError: Error when in A_x the A(thing) is not A or B and 
+                if the x(ting) is not a number smaller than 8.
+        """
+
+        if not isinstance(pin_num, str):
+            raise TypeError("Wrong type of pin_num, must be str.")
+
+        # Check format of pin_num
+        pin_num = pin_num.split('_')
+        if len(pin_num) != 2:
+            raise ValueError("Wrong format of pin_num, should be A_x or B_x")
+
+        chunk = pin_num[0]
+        if chunk != 'A' and chunk != 'B':
+            raise ValueError("Wrong value of 'chunk', must be A or B")
+
+        if pin_num[1].isnumeric():
+            number = int(pin_num[1])
+            if number > 7:
+                raise ValueError("Wrong pin number, it must be [0, 7]")
+        else:
+            raise TypeError("Wrong type of A_x, x must be int")
+        
+        return chunk, number
+
+    def set_pin_dir(self, pin_num, function):
         """Set pin direction
 
         Args:
-            pin_num: The number of pin [0, 7]
+            pin_num (str): The pin number in format A_x or B_x, where A/B is 
+                the pin-chunk and x is the number. See modules's datasheet.
             function: Boolean, it could be 1 for input and 0 for output.
-            chunk: From which team of pins to use. Could be 'A'
         """
         
+        chunk, pin_num = self._get_chunk_number(pin_num)
         address = self.IODIRA if chunk is 'A' else self.IODIRB
         self._set_bit_register(address, pin_num+1, int(function))
 
-    def set_pin_pol(self, pin_num, polarity, chunk='A'):
+    def set_pin_pol(self, pin_num, polarity):
         """Set pin polarity
 
         Args:
-            pin_num: The number of pin [0, 7]
-            Polarity (boolean): It could be 1 for reverse and 0 for same. 
-            chunk: From which team of pins to use. Could be 'A'
+            pin_num (str): The pin number in format A_x or B_x, where A/B is 
+                the pin-chunk and x is the number. See modules's datasheet.
+            polarity (boolean): It could be 1 for reverse and 0 for same. 
         """
 
+        chunk, pin_num = self._get_chunk_number(pin_num)
         address = self.IPOLA if chunk is 'A' else self.IPOLB
         self._set_bit_register(address, pin_num+1, int(polarity))
 
-    def set_pin_int(self, pin_num, interrupt, chunk='A'):
+    def set_pin_int(self, pin_num, interrupt):
         """Set pin interrupt on change.
 
         In order to work the DEFVAL and INTCON registers must be set.
 
         Args:
-            pin_num: The number of pin [0, 7]
+            pin_num (str): The pin number in format A_x or B_x, where A/B is 
+                the pin-chunk and x is the number. See modules's datasheet.
             interrupt: Boolean representing the interrupt status of the pin.
-            chunk: From which team of pins to use. Could be 'A'
         """
 
+        chunk, pin_num = self._get_chunk_number(pin_num)
         address = self.GPINTENA if chunk is 'A' else self.GPINTENB
         self._set_bit_register(address, pin_num+1, int(interrupt))
 
-    def set_pin_def_val(self, pin_num, def_val, chunk='A'):
+    def set_pin_def_val(self, pin_num, def_val):
         """Set pin default value for comparison.
 
         The value of each bits will be compared with the value of the associate
         pin and if they are different then an interrupt will happen.
 
         Args:
-            pin_num: The number of pin [0, 7]
+            pin_num (str): The pin number in format A_x or B_x, where A/B is 
+                the pin-chunk and x is the number. See modules's datasheet.
             def_val: Int representing the compare value. Should be 0 or 1.
-            chunk: From which team of pins to use. Could be 'A'
         """
 
+        chunk, pin_num = self._get_chunk_number(pin_num)
         address = self.DEFVALA if chunk is 'A' else self.DEFVALB
         self._set_bit_register(address, pin_num+1, int(def_val))
 
-    def set_pin_intcon(self, pin_num, value, chunk='A'):
+    def set_pin_intcon(self, pin_num, value):
         """Set pin intcon value.
 
         If the corresponding pin's bit is set the the value is compared with 
@@ -101,11 +139,12 @@ class MCP23x17(Device):
         previous value.
 
         Args:
-            pin_num: The number of pin [0, 7]
+            pin_num (str): The pin number in format A_x or B_x, where A/B is 
+                the pin-chunk and x is the number. See modules's datasheet.
             value: Int representing the value. Should be 0 or 1.
-            chunk: From which team of pins to use. Could be 'A'
         """
 
+        chunk, pin_num = self._get_chunk_number(pin_num)
         address = self.INTCONA if chunk is 'A' else self.INTCONB
         self._set_bit_register(address, pin_num+1, value)
     
@@ -189,98 +228,105 @@ class MCP23x17(Device):
 
         self._set_bit_register(self.IOCON, 2, value)
 
-    def set_pin_pull_up(self, pin_num, pull, chunk='A'):
+    def set_pin_pull_up(self, pin_num, pull):
         """Set the pull up of a pin.
 
         Args:
-            pin_num: The number of pin [0, 7]
+            pin_num (str): The pin number in format A_x or B_x, where A/B is 
+                the pin-chunk and x is the number. See modules's datasheet.
             pull (boolean): It could be 0 for down and 1 for up.
-            chunk: From which team of pins to use. Could be 'A'
         """
 
+        chunk, pin_num = self._get_chunk_number(pin_num)
         address = self.GPPUA if chunk is 'A' else self.GPPUB
         self._set_bit_register(address, pin_num+1, int(pull))
 
-    def get_intf(self, pin_num, chunk='A'):
+    def get_intf(self, pin_num):
         """Get the pin interrupt flag.
 
         It reflects if the pin caused the interrupt.
         
         Args:
-            pin_num: The number of pin [0, 7]
-            chunk: From which team of pins to use. Could be 'A'
+            pin_num (str): The pin number in format A_x or B_x, where A/B is 
+                the pin-chunk and x is the number. See modules's datasheet.
 
         Returns:
             The flag value.
         """
 
+        chunk, pin_num = self._get_chunk_number(pin_num)
         address = self.INTFA if chunk is 'A' else self.INTFB
 
         return self._get_bit_register(address, pin_num+1)
 
-    def get_intcap(self, pin_num, chunk='A'):
+    def get_intcap(self, pin_num):
         """Get the pin's state when the interrupt occured.
 
         Args:
-            pin_num: The number of pin [0, 7]
-            chunk: From which team of pins to use. Could be 'A'
+            pin_num (str): The pin number in format A_x or B_x, where A/B is 
+                the pin-chunk and x is the number. See modules's datasheet.
 
         Returns:
             The flag value.
         """
 
+        chunk, pin_num = self._get_chunk_number(pin_num)
         address = self.INTCAPA if chunk is 'A' else self.INTCAPB
 
         return self._get_bit_register(address, pin_num+1)
 
-    def read(self, pin_num, chunk='A'):
+    def read(self, pin_num):
         """Read the pins state.
         
         Args:
-            pin_num: The number of pin [0, 7]
-            chunk: From which team of pins to use. Could be 'A'
+            pin_num (str): The pin number in format A_x or B_x, where A/B is 
+                the pin-chunk and x is the number. See modules's datasheet.
         
         Returns:
             The pin's state.
         """
 
+        chunk, pin_num = self._get_chunk_number(pin_num)
         address = self.GPIOA if chunk is 'A' else self.GPIOB
 
         return self._get_bit_register(address, pin_num+1)
 
-    def write(self, pin_num, value, chunk='A'):
+    def write(self, pin_num, value):
         """Write to the pin
         
         Args:
-            pin_num: The number of pin [0, 7]
+            pin_num (str): The pin number in format A_x or B_x, where A/B is 
+                the pin-chunk and x is the number. See modules's datasheet.
             value: Int could be 0 or 1.
-            chunk: From which team of pins to use. Could be 'A'
         """
 
+        chunk, pin_num = self._get_chunk_number(pin_num)
         address = self.GPIOA if chunk is 'A' else self.GPIOB
         self._set_bit_register(address, pin_num+1, value)
     
-    def read_olat(self, pin_num, chunk='A'):
+    def read_olat(self, pin_num):
         """Read the olat register.
 
         Args:
-            pin_num: The number of pin [0, 7]
-            chunk: From which team of pins to use. Could be 'A'
+            pin_num (str): The pin number in format A_x or B_x, where A/B is 
+                the pin-chunk and x is the number. See modules's datasheet.
         """
 
+        chunk, pin_num = self._get_chunk_number(pin_num)
         address = self.OLATA if chunk is 'A' else self.OLATB
 
         return self._get_bit_register(address, pin_num+1)
 
-    def write_olat(self, pin_num, value, chunk='A'):
+    def write_olat(self, pin_num, value):
         """Write to the pin olat
         
         Args:
-            pin_num: The number of pin [0, 7]
+            pin_num (str): The pin number in format A_x or B_x, where A/B is 
+                the pin-chunk and x is the number. See modules's datasheet.
             value: Int could be 0 or 1.
-            chunk: From which team of pins to use. Could be 'A'
         """
 
+        chunk, pin_num = self._get_chunk_number(pin_num)
         address = self.OLATA if chunk is 'A' else self.OLATB
         self._set_bit_register(address, pin_num+1, value)
 
