1d02f312faa00a9a5bc5229767c2236a55e41002
Iasonas Paraskevopoulos <iaswnparaskev@gmail.com>
2019-10-03 08:44:05 +0100

    Add mcp files.

--- pidevices/__init__.py
+++ pidevices/__init__.py
@@ -1,6 +1,8 @@
-name = 'pidevices'
-
 from .sensors import *
 from .actuators import *
 from .hardware_interfaces import *
 from .devices import *
+from .mcp23x17 import *
+from .mcp23017 import *
+
+name = 'pidevices'
