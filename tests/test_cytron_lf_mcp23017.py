1135f85c65f747855a07b353ebbaead2669b1a06
Iasonas Paraskevopoulos <iaswnparaskev@gmail.com>
2019-10-24 10:21:20 +0100

    Modify lf tests

--- tests/test_cytron_lf_mcp23017.py
+++ tests/test_cytron_lf_mcp23017.py
@@ -6,7 +6,8 @@ from pidevices import CytronLfLSS05Mcp23017
 class TestCytronLf(unittest.TestCase):
 
     def test_start(self):
-        lf = CytronLfLSS05Mcp23017('B_0', 'B_1', 'B_2', 'B_3', 'B_4', cal='B_5')
+        lf = CytronLfLSS05Mcp23017('B_0', 'B_1', 'B_2', 'B_3', 'B_4', cal='B_5',
+                                   bus=3, address=0x22)
 
         number = 'B_0'
         self.assertEqual(lf.so_1, number, "Should be {}".format(number))
@@ -24,8 +25,8 @@ class TestCytronLf(unittest.TestCase):
         lf.stop()
 
     def test_mode(self):
-        lf = CytronLfLSS05Mcp23017('B_0', 'B_1', 'B_2', 'B_3', 'B_4', cal='B_5')
-        
+        lf = CytronLfLSS05Mcp23017('B_0', 'B_1', 'B_2', 'B_3', 'B_4', cal='B_5',
+                                   bus=3, address=0x22)
         print("Bright mode")
         lf.mode = 'bright'
         time.sleep(5)
@@ -34,12 +35,14 @@ class TestCytronLf(unittest.TestCase):
         lf.stop()
 
     def test_calibrate(self):
-        lf = CytronLfLSS05Mcp23017('B_0', 'B_1', 'B_2', 'B_3', 'B_4', cal='B_5')
+        lf = CytronLfLSS05Mcp23017('B_0', 'B_1', 'B_2', 'B_3', 'B_4', cal='B_5',
+                                   bus=3, address=0x22)
 
         lf.calibrate()
 
     def test_read(self):
-        lf = CytronLfLSS05Mcp23017('B_0', 'B_1', 'B_2', 'B_3', 'B_4', cal='B_5')
+        lf = CytronLfLSS05Mcp23017('B_0', 'B_1', 'B_2', 'B_3', 'B_4', cal='B_5',
+                                   bus=3, address=0x22)
 
         for i in range(20):
             print(lf.read())
